3218c6376c14ffbab5b82e49764457d0bc518579
Jegadesh Muthu B <jegadesh213@gmail.com>
2026-05-23 20:07:12 +0530

    23052026

---
+++ demoproject/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <div style={{
+      background: "#1e293b",
+      color: "white",
+      padding: "15px"
+    }}>
+      ERP System
+    </div>
+  );
+}
